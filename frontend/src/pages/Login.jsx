129df3b14375cf81ed5ff1bf32b8c215a18dcaea
Squiford <edisonfranco2566@gmail.com>
2023-12-20 14:40:41 -0300

    a

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = ({ onLogin }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://3.84.128.5:3001/api/login', {
+            const response = await fetch('http://172-31-27-126:3001/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
